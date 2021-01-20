bb9b0b3bb0586a7e5970e1c568ea67087703b2e6
adarsh1997shah <adarshshah01@gmail.com>
2021-01-20 14:21:16 +0530

    fix style

--- src/App.js
+++ src/App.js
@@ -7,106 +7,106 @@ import { calculateWinner } from './helpers';
 import './styles/root.scss';
 
 const NEW_GAME = [
-    {
-        board: Array(9).fill(null),
-        isplayer1: true,
-    },
+  {
+    board: Array(9).fill(null),
+    isplayer1: true,
+  },
 ];
 
 const App = () => {
-    const [records, setRecord] = useState(NEW_GAME);
-    const [currentMove, setCurrentMove] = useState(0);
-    const [isSetBoard, setIsSetBoard] = useState(false);
+  const [records, setRecord] = useState(NEW_GAME);
+  const [currentMove, setCurrentMove] = useState(0);
+  const [isSetBoard, setIsSetBoard] = useState(false);
 
-    const current = records[currentMove];
-    const { winner, winningSquare } = calculateWinner(current.board);
+  const current = records[currentMove];
+  const { winner, winningSquare } = calculateWinner(current.board);
 
-    // Calculating whose turn should be next.
-    const handleTurn = e => {
-        const { index } = e.currentTarget.dataset;
-        const currentBoard = [...current.board];
+  // Calculating whose turn should be next.
+  const handleTurn = e => {
+    const { index } = e.currentTarget.dataset;
+    const currentBoard = [...current.board];
 
-        if (isSetBoard) {
-            console.log('Please press ok button to start from this move');
-            return;
-        }
+    if (isSetBoard) {
+      console.log('Please press ok button to start from this move');
+      return;
+    }
 
-        // Check for empty board square
-        if (currentBoard[index] || winner) {
-            return;
-        }
+    // Check for empty board square
+    if (currentBoard[index] || winner) {
+      return;
+    }
 
-        if (current.isplayer1) {
-            currentBoard[index] = 'X';
-        } else {
-            currentBoard[index] = 'O';
-        }
+    if (current.isplayer1) {
+      currentBoard[index] = 'X';
+    } else {
+      currentBoard[index] = 'O';
+    }
 
-        // Updating the records.
-        const newRecords = records.concat({
-            board: [...currentBoard],
-            isplayer1: !current.isplayer1,
-        });
+    // Updating the records.
+    const newRecords = records.concat({
+      board: [...currentBoard],
+      isplayer1: !current.isplayer1,
+    });
 
-        setRecord(newRecords);
-        setCurrentMove(currentMove + 1);
-    };
+    setRecord(newRecords);
+    setCurrentMove(currentMove + 1);
+  };
 
-    const handleSetBoard = e => {
-        setCurrentMove(parseInt(e.currentTarget.id, 10));
-        setIsSetBoard(true);
-    };
+  const handleSetBoard = e => {
+    setCurrentMove(parseInt(e.currentTarget.id, 10));
+    setIsSetBoard(true);
+  };
 
-    const handleRecordBoard = () => {
-        if (isSetBoard) {
-            const newRecords = records.slice(0, currentMove + 1);
+  const handleRecordBoard = () => {
+    if (isSetBoard) {
+      const newRecords = records.slice(0, currentMove + 1);
 
-            setRecord(newRecords);
-            setIsSetBoard(false);
-        }
-    };
+      setRecord(newRecords);
+      setIsSetBoard(false);
+    }
+  };
 
-    const handleNewGame = () => {
-        setRecord(NEW_GAME);
-        setCurrentMove(0);
-    };
+  const handleNewGame = () => {
+    setRecord(NEW_GAME);
+    setCurrentMove(0);
+  };
 
-    return (
-        <div className="app">
-            <h1>
-                TIC <span className="text-green">TAC</span>{' '}
-                <span className="text-orange">TOE</span>
-            </h1>
-            <StatusMessage winner={winner} current={current} />
-            <Board
-                board={current.board}
-                handleTurn={handleTurn}
-                winningSquare={winningSquare}
-            />
-            <button
-                type="button"
-                className={`btn-reset ${winner ? 'active' : ''}`}
-                onClick={handleNewGame}
-            >
-                Start new Game
-            </button>
-            <h3>Press OK to go to a particular move.</h3>
-            <Record
-                records={records}
-                handleSetBoard={handleSetBoard}
-                currentMove={currentMove}
-            />
-            <button
-                type="button"
-                className="btn-reset"
-                onClick={handleRecordBoard}
-                style={{ marginTop: '0', marginBottom: '25px' }}
-            >
-                OK
-            </button>
-            <div className="bg-balls" />
-        </div>
-    );
+  return (
+    <div className="app">
+      <h1>
+        TIC <span className="text-green">TAC</span>{' '}
+        <span className="text-orange">TOE</span>
+      </h1>
+      <StatusMessage winner={winner} current={current} />
+      <Board
+        board={current.board}
+        handleTurn={handleTurn}
+        winningSquare={winningSquare}
+      />
+      <button
+        type="button"
+        className={`btn-reset ${winner ? 'active' : ''}`}
+        onClick={handleNewGame}
+      >
+        Start new Game
+      </button>
+      <h3>Press OK to go to a particular move.</h3>
+      <Record
+        records={records}
+        handleSetBoard={handleSetBoard}
+        currentMove={currentMove}
+      />
+      <button
+        type="button"
+        className="btn-reset"
+        onClick={handleRecordBoard}
+        style={{ marginTop: '0', marginBottom: '25px' }}
+      >
+        OK
+      </button>
+      <div className="bg-balls" />
+    </div>
+  );
 };
 
 export default App;
--- src/components/Record.js
+++ src/components/Record.js
@@ -1,28 +1,26 @@
 import React from 'react';
 
 const Record = ({ records, handleSetBoard, currentMove }) => {
-    return (
-        <div className="history-wrapper">
-            <ul className="history">
-                {records.map((record, index) => {
-                    return (
-                        <li key={index}>
-                            <button
-                                className={`btn-move ${
-                                    currentMove === index ? 'active' : ''
-                                }`}
-                                id={index}
-                                type="button"
-                                onClick={handleSetBoard}
-                            >
-                                #{index} move
-                            </button>
-                        </li>
-                    );
-                })}
-            </ul>
-        </div>
-    );
+  return (
+    <div className="history-wrapper">
+      <ul className="history">
+        {records.map((record, index) => {
+          return (
+            <li key={index}>
+              <button
+                className={`btn-move ${currentMove === index ? 'active' : ''}`}
+                id={index}
+                type="button"
+                onClick={handleSetBoard}
+              >
+                #{index} move
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default Record;
